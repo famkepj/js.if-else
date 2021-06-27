c9d42a0e320cd9c1f4d332cc75ee1792600dcc00
famkepj <82056762+famkepj@users.noreply.github.com>
2021-06-27 18:41:59 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+const age = 20;
+if (age > 17){
+    console.log ("Welom in onze kroeg");
+} else{
+    console.log("Helaas ben je nog geen 18 en mag ik je niet binnen laten");
+}
+
+const isFemale = false;
+if (isFemale){
+    console.log("welkom op de ladiesnight");
+} else{
+    console.log("Het is ladiesnight, kies een andere datum");
+}
+
+const driverstatus = "box";
+
+if (driverstatus == "bob"){
+    console.log("Rij veilig naar huis");
+} else{
+    console.log("Je mag niet meer autorijden, bestel een taxi");
+}
